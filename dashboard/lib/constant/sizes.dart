6a1f4385ee1a0bfa77b04acfcc002727ee5a655b
Mayar AB <mab026550@gmail.com>
2023-05-02 00:56:01 +0300

    I make the screens size with Inches as it should instad of pixels and correct the sizes fo screenns

--- dashboard/lib/constant/sizes.dart
+++ dashboard/lib/constant/sizes.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Sizes {
-  GetDeviceType getDeviceType = GetDeviceType();
-
   late double appBarIconSize;
   late double appBarTextSize;
   late double width;
@@ -14,6 +12,7 @@ class Sizes {
   Sizes(this.context) {
     width=MediaQuery.of(context).size.width;
     height=MediaQuery.of(context).size.height;
+    GetDeviceType getDeviceType = GetDeviceType();
     if (getDeviceType.getDevicetype() == 'computer') {
       setSizesForComputer();
     } else if (getDeviceType.getDevicetype() == 'tablet') {
@@ -23,27 +22,33 @@ class Sizes {
     }
   }
   void setSizesForMobile() {
+    print('set size for mobile');
     appBarIconSize = Get.size.height * .1;
     appBarTextSize = width * .1;
   }
 
   void setSizesForComputer() {
+    print('set size for computer');
     appBarIconSize = Get.height * .05;
     appBarTextSize = width * .02;
   }
 
   void setSizesForTablet() {
+    print('set size for tablet');
     appBarIconSize = Get.size.height * .1;
     appBarTextSize = Get.size.width * .1;
   }
 }
 
 class GetDeviceType {
+
   String getDevicetype() {
-    double diagonal = sqrt(pow(Get.size.width, 2) + pow(Get.size.height, 2));
-    if (diagonal >= 10.1) {
+    double diagonal = sqrt(pow(Get.size.width, 2) + pow(Get.size. height, 2))/96;//the 96 because we use the pixel when we should use inche and 1 inche=96pixel
+    print(diagonal);
+    print(Get.size.height);
+    if (diagonal >= 9.1||Get.size.height/96>=7) {
       return 'computer';
-    } else if (diagonal >= 7.0) {
+    } else if (diagonal >= 8.5 && Get.size.height/96 >= 5) {
       return 'tablet';
     } else {
       return 'smart phone';
