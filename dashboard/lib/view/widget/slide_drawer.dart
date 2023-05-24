c6598cdc8aa31314a7efab47d05c2ad9fa5e98f2
Mayar AB <mab026550@gmail.com>
2023-05-24 19:50:34 +0300

    admin main page almost done.

--- dashboard/lib/view/widget/slide_drawer.dart
+++ dashboard/lib/view/widget/slide_drawer.dart
@@ -1,7 +1,10 @@
+import 'package:dashboard/constant/font.dart';
 import 'package:dashboard/main.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../constant/sizes.dart';
 import '../../constant/theme.dart';
+import '../Screens/setting/setting_page.dart';
 
 // ignore: must_be_immutable
 class SlideDrawer extends StatelessWidget {
@@ -9,6 +12,7 @@ class SlideDrawer extends StatelessWidget {
   SlideDrawerController controller = Get.put(SlideDrawerController());
   @override
   Widget build(BuildContext context) {
+    Sizes size = Sizes(context);
     return Obx(
       () => AnimatedContainer(
         duration: const Duration(milliseconds: 150),
@@ -24,40 +28,44 @@ class SlideDrawer extends StatelessWidget {
                 controller.changeDrawerStateWhenHover();
               },
               child: AnimatedContainer(
-                color: Get.isDarkMode ? backGroundDarkColor : skinColorWhite,
+                color: Get.isDarkMode
+                    ? backGroundDarkColor.withOpacity(0.2)
+                    : skinColorWhite,
                 duration: const Duration(milliseconds: 150),
                 width: controller.setWidth(50, 240),
                 height: Get.size.height,
+                child: drawerChildren(context, size),
               ),
             ),
             AnimatedPositioned(
               duration: const Duration(milliseconds: 150),
               top: 60,
               left: sharedPreferences!.getString('lang') == 'en'
-                  ? controller.setWidth(40, 225)
-                  : 0,
+                  ? controller.setWidth(33, 225)
+                  : sharedPreferences!.getString('lang') == 'ar'
+                      ? null
+                      : controller.setWidth(33, 225),
               right: sharedPreferences!.getString('lang') == 'ar'
-                  ? controller.setWidth(40, 225)
-                  : 0,
-              // left: controller.setWidth(40, 225),
+                  ? controller.setWidth(33, 225)
+                  : null,
               child: InkWell(
-                // BoxShape.highlightShape: BoxShape.circle,
                 onTap: () {
                   controller.changeDrawerState();
                 },
                 child: Container(
-                  height: Get.size.height * .1,
+                  height: Get.size.height * .04,
                   decoration: BoxDecoration(
                       boxShadow: [
                         BoxShadow(
                             offset: const Offset(0.4, 0.9),
                             color: Get.isDarkMode
-                                ? Colors.white70
+                                ? const Color.fromARGB(255, 24, 24, 24)
                                 : Colors.black12,
                             spreadRadius: 1)
                       ],
-                      color:
-                          Get.isDarkMode ? backGroundDarkColor : skinColorWhite,
+                      color: Get.isDarkMode
+                          ? backGroundDarkColor.withOpacity(0.1)
+                          : skinColorWhite,
                       shape: BoxShape.circle),
                   child: Icon(
                     controller.isCliked.value
@@ -70,6 +78,108 @@ class SlideDrawer extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget drawerChildren(BuildContext context, Sizes size) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: Column(children: [
+        SizedBox(height: Get.size.height * .02),
+        mainDrawerChid(context, size),
+        Divider(),
+        SizedBox(height: Get.size.height * .02),
+        firstDrawerChid(context, size),
+        SizedBox(height: Get.size.height * .02),
+        secondDrawerChid(context),
+      ]),
+    );
+  }
+
+  Widget mainDrawerChid(BuildContext context, Sizes size) {
+    return Row(children: [
+      Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: SizedBox(
+            width: 32,
+            child: ClipOval(
+                child: Image.asset('assets/images/The project icon.jpg'))),
+      ),
+      Visibility(
+        replacement: const Text(''),
+        visible: controller.isCliked.value || controller.isHover.value,
+        child: Expanded(
+          child: Text(
+            'Dar abdallah',
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Get.isDarkMode ? skinColorWhite : backGroundDarkColor,
+                fontFamily: jostFontFamily),
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  Widget firstDrawerChid(BuildContext context, Sizes size) {
+    return MaterialButton(
+      onPressed: () {},
+      child: Row(children: [
+        Icon(
+          Icons.home,
+          color: Get.isDarkMode ? darkPrimaryColor : primaryColor,
+        ),
+        Visibility(
+          replacement: const Text(''),
+          visible: controller.isCliked.value || controller.isHover.value,
+          child: Expanded(
+            child: Text(
+              'Home',
+              style: TextStyle(
+                  color: Get.isDarkMode ? skinColorWhite : backGroundDarkColor,
+                  fontFamily: jostFontFamily),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget secondDrawerChid(BuildContext context) {
+    return MaterialButton(
+      onPressed: () {
+        showSettingsDialog(context);
+      },
+      child: Row(children: [
+        Icon(Icons.settings,
+            color: Get.isDarkMode ? darkPrimaryColor : primaryColor),
+        Visibility(
+          replacement: const Text(''),
+          visible: controller.isCliked.value || controller.isHover.value,
+          child: Expanded(
+            child: Text(
+              'Settings',
+              style: TextStyle(
+                  color: Get.isDarkMode ? skinColorWhite : backGroundDarkColor,
+                  fontFamily: jostFontFamily),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  void showSettingsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          clipBehavior: Clip.antiAlias,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          child: Setting(),
+        );
+      },
     );
   }
 }
